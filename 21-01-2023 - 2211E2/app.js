a5ceaedfc7e0534cc6cad93120d5433e47fe8e3a
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-01-21 17:15:50 +0500

    21-01-2023

---
+++ 21-01-2023 - 2211E2/app.js
@@ -0,0 +1,83 @@
+// let alpha=prompt("Enter any alphabet")
+// function check(al){
+// switch(al.toLowerCase()){
+//     case "a":
+//         alert("vowel")
+//         break
+//     case "e":
+//         alert("Vowel")
+//         break
+//     case "i":
+//         alert("Vowel")
+//         break
+//     case "o":
+//         alert("Vowel")
+//         break
+//     case "u":
+//         alert("Vowel")
+//         break
+// default:
+//     alert("consonant")
+//     break
+
+// }
+// }
+
+
+// check(alpha)
+
+// let stdData1={
+//     name:"Arham",
+//     batch:"2211E2",
+//     roll:"07"
+// }
+// let stdData2={
+// name:"Ali",
+// batch:"2210B1",
+// roll:"08"
+// }
+// alert(stdData1.batch)
+
+// let basic={
+// monthly:"$9.99",
+// diskSpace:"100 GB",
+// dataTransfer:"1000GB",
+// sitePages:10
+// }
+// let pro={
+//     monthly:"$9.99",
+//     diskSpace:"100 GB",
+//     dataTransfer:"1000GB",
+//     sitePages:50
+// }
+
+// let ult={
+//     monthly:"$9.99",
+//     diskSpace:"100 GB",
+//     dataTransfer:"1000GB",
+//     sitePages:500
+// }
+
+
+// alert("Professional has "+(ult.sitePages-pro.sitePages)+" less than ultimate")
+// document.write("You will have "+ult.sitePages+" site pages for "+ult.monthly+" payment")
+
+// for(let key in ult){
+//     alert(ult[key])
+// }
+
+// let newarr=Object.values(ult)
+// alert(newarr)
+
+let date=new Date()
+
+// alert(date.getDay())
+// alert(date.getFullYear())
+// alert(date.getMonth())
+// alert(date.getDate())
+// alert((date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear())
+// alert(date.getHours())
+// alert(date.getMinutes())
+// alert(date.getSeconds())
+// document.write(date.getMilliseconds())
+document.write(date.getTime())
